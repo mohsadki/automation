48579270f35ec9345573373237c96f526582f81f
msed <mohsadki@gmail.com>
2021-11-14 22:51:22 +0100

    errno

--- code/index.php
+++ code/index.php
@@ -1,7 +1,7 @@
 <?php
 
 $link = mysql_connect("172.17.0.4:3306", "root", "password")
-    or die("Impossible de se connecter : " . mysql_error());
+    or die("Impossible de se connecter : " . mysql_error() . mysql_errno() );
 echo 'Connexion réussie';
 
 $query = sprintf("INSERT INTO `log` (`id`, `ts`, `mess`) VALUES (NULL, CURRENT_TIMESTAMP, 'first message test')");
